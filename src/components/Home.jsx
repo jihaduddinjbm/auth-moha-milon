827cd223c8551e2a815390a9d82e84bd98b44bab
jihaduddinjbm <jihaduddin117@gmail.com>
2023-10-08 23:17:17 +0600

    added the all created jsx file mine

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+
+
+
+const Home = () => {
+   
+    return (
+        <div>
+            <h2 className="text-2xl">ohhh man for:</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Login.jsx
@@ -0,0 +1,74 @@
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
+
+
+const Login = () => {
+    const {signInUser, signInWithGoogle} = useContext(AuthContext);
+    const Navigate = useNavigate();
+    const handleLogin = e =>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password)
+
+        signInUser(email, password)
+        .then( result =>{
+            console.log(result.user)
+            e.target.reset();
+            Navigate('/');
+        })
+        .catch( error =>{
+            console.error(error)
+        })
+    }
+    const handleGoogleSignIn = () =>{
+        signInWithGoogle()
+        .then( result =>{
+            console.log(result.user)
+        })
+        .catch(error =>{
+            console.error(error)
+        })
+    }
+
+    return (
+        <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col">
+    <div className="text-center">
+      <h1 className="text-5xl font-bold">Login now!</h1>
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form onSubmit={handleLogin}>
+      <div className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" name="email" placeholder="email" required className="input input-bordered"  />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" name="password" placeholder="password" required className="input input-bordered"  />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </div>
+      </form>
+      <p>new? please <Link to="/register">
+      <button className="btn btn-link">register</button>
+      </Link></p>
+      <p><button onClick={handleGoogleSignIn} className="btn btn-ghost">Google</button></p>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Login;
